9710f224e4f480d86b50834d4b6cfb93e7ccf1bb
Gerz2011 <30986978+gerz2011@users.noreply.github.com>
2019-06-11 17:58:46 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import telebot
+import math
 from telebot import apihelper
 from telebot import types
 from data import price_iphone_x
@@ -12,23 +13,30 @@ TOKEN = '781098537:AAEGQ7-kRv6Pt8KGs5CfW9RiPRLU8lKHp58'
 bot = telebot.TeleBot(TOKEN)
 
 
-# apihelper.proxy = {'https':'https://188.216.77.95:8118'}
+apihelper.proxy = {'https':'https://188.216.77.95:8118'}
 
-model = 'iphone_ipad_wath_macbook'.split('_')
-link = 'Мы на крте_Контакты_Чат'.split('_')
-problem = 'Дисплей ориг_Замена стекла_Копия ААА_Аккумулятор_Защитное стекло'.split('_')
-problem1 = 'Дисплей ориг_Замена стекла_Копия ААА_Заднее стекло_'
+main_menu = 'iphone_ipad_wath_macbook_Наши контакты'.split('_')
+
+problem_x = 'Дисплей ориг_Замена стекла_Заднее стекло_Аккумулятор_Защитное стекло'.split('_')
+problem_8 = 'Дисплей ориг_Замена стекла_Заднее стекло_Копия ААА_Аккумулятор_Защитное стекло'.split('_')
+problem_7 = 'Дисплей ориг_Замена стекла_Копия ААА_Аккумулятор_Защитное стекло'.split('_')
+problem_ipad = 'Замена стекла_Замена модуля'.split('_')
 
 iphone = 'Xs Max_Xs_Xr_X_8 Plus_8_7 Plus_7_6s Plus_6s_6_Plus_6_SE_5 серия'.split('_')
+
 ipad = list(price_ipad)
 
 contact = f'Выездной ремонт iPhone "PROFIPHONE"\nтел: 8 931 340 38 33\nтел: 8 (812) 648-59-51\nадес: м. Сенная, Московский пр. д2'
 
 
-def text_massage(model, cl):
+def text_massage(model, price_iphone):
+    if price_iphone == price_iphone_x: problem = problem_x
+    if price_iphone == price_iphone_8: problem = problem_8
+    if price_iphone == price_iphone_7: problem = problem_7
+    if price_iphone == price_ipad: problem = problem_ipad
     num = 0
     massage = []
-    for key in cl[model]:
+    for key in price_iphone[model]:
         massage.append(f'{problem[num]} - {key} руб.\n')
         num = num + 1
     massage.append('---\nтел - 89313403833\nТелеграм - @gerz_og')
@@ -41,8 +49,9 @@ def app_btn(arr):
     array = []
     for i in arr:
         w = 0
-        for iter in range(round(len(i) / 4)):
+        for iter in range(math.ceil(len(i) / 4)):
             array.append(i[w:w+4])
+
             w = w + 4
     for i in array:
         markup.row(*[types.KeyboardButton(name) for name in i])
@@ -51,7 +60,7 @@ def app_btn(arr):
 
 @bot.message_handler(commands=['start'])
 def get_id(m):
-    mt = app_btn([model, link])
+    mt = app_btn([main_menu])
     bot.send_message(m.from_user.id, "выберите устройство:", reply_markup=mt)
     
 
@@ -61,9 +70,9 @@ def answe(m):
     if m.text == 'назад':
         mt = types.ReplyKeyboardRemove()
         bot.send_message(m.from_user.id, "назад", reply_markup=mt)
-        mt = app_btn([model, link])
+        mt = app_btn([main_menu])
         bot.send_message(m.from_user.id, "выберите устройство:", reply_markup=mt)
-    if m.text == 'Контакты':
+    if m.text == 'Наши контакты':
         mt = types.ReplyKeyboardRemove()
         markup = types.InlineKeyboardMarkup()
         btn_my_site= types.InlineKeyboardButton(text='Наш официальный сайт', url='https://profiphone.ru/')
@@ -88,25 +97,29 @@ def answe(m):
         mt = app_btn([ipad])
         mt.row(types.KeyboardButton('назад'))
         bot.send_message(m.from_user.id, "выберите модель:", reply_markup=mt)
-    if m.text == 'wach':
-        mt = app_btn([ipad])
-        mt.row(types.KeyboardButton('назад'))
-        bot.send_message(m.from_user.id, "выберите модель:", reply_markup=mt)
-    if m.text == 'macbook':
-        mt = app_btn([ipad])
-        mt.row(types.KeyboardButton('назад'))
-        bot.send_message(m.from_user.id, "выберите модель:", reply_markup=mt)
+    # if m.text == 'wach':
+    #     mt = app_btn([wach])
+    #     mt.row(types.KeyboardButton('назад'))
+    #     bot.send_message(m.from_user.id, "выберите модель:", reply_markup=mt)
+    # if m.text == 'macbook':
+    #     mt = app_btn([macbook])
+    #     mt.row(types.KeyboardButton('назад'))
+    #     bot.send_message(m.from_user.id, "выберите модель:", reply_markup=mt)
 
 ###############################################################################
 ##прайс с ценами
     if m.text in price_iphone_x:
         bot.send_message(m.from_user.id, text_massage(m.text, price_iphone_x))
+        print(m.text)
     if m.text in price_iphone_8:
         bot.send_message(m.from_user.id, text_massage(m.text, price_iphone_8))
+        print(m.text)
     if m.text in price_iphone_7:
         bot.send_message(m.from_user.id, text_massage(m.text, price_iphone_7))
+        print(m.text)
     if m.text in price_ipad:
         bot.send_message(m.from_user.id, text_massage(m.text, price_ipad))
+        print(m.text)
 
 
 
